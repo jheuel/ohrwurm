fa17ed75f6becbbfa62159e1b1b6b8eb56f73035
Johannes Heuel <johannes.heuel@rwth-aachen.de>
2024-06-18 10:12:48 +0200

    add error message for premium tracks

--- src/colors.rs
+++ src/colors.rs
@@ -1,2 +1,20 @@
+#[allow(dead_code)]
 pub(crate) const BLURPLE: u32 = 0x58_65_F2;
+#[allow(dead_code)]
 pub(crate) const YELLOW: u32 = 0xFE_E7_5C;
+#[allow(dead_code)]
+pub(crate) const WHITE: u32 = 0xFF_FF_FF;
+#[allow(dead_code)]
+pub(crate) const GREYPLE: u32 = 0x99_AA_B5;
+#[allow(dead_code)]
+pub(crate) const BLACK: u32 = 0x23_27_2A;
+#[allow(dead_code)]
+pub(crate) const DARK_BUT_NOT_BLACK: u32 = 0x2C_2F_33;
+#[allow(dead_code)]
+pub(crate) const NOT_QUITE_BLACK: u32 = 0x23_27_2A;
+#[allow(dead_code)]
+pub(crate) const GREEN: u32 = 0x57_F2_87;
+#[allow(dead_code)]
+pub(crate) const FUCHSIA: u32 = 0xEB_45_9E;
+#[allow(dead_code)]
+pub(crate) const RED: u32 = 0xED_42_45;
